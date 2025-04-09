ca3c2aef327d904633aeb32b47b55000ed9a2cc7
Mazakov-d <mazaridorian5@gmail.com>
2025-04-09 13:48:07 +0200

    modifing the header

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: mazakov <mazakov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 10:20:41 by mazakov           #+#    #+#             */
-/*   Updated: 2025/04/09 13:20:55 by mazakov          ###   ########.fr       */
+/*   Updated: 2025/04/09 13:47:42 by mazakov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,39 +32,8 @@ typedef struct s_env
 }	t_env;
 
 typedef struct s_data
-
-int	ft_strcmp(char *s1, char *s2)
 {
-	int	i;
-
-	i = 0;
-	if (!s2)
-		return (0);
-	while (s1 && s2 && s1[i] == s2[i])
-		i++;
-	if (!s1[i] && !s2[i])
-		return (2);
-	if (!s2[i])
-		return (1);
-	return (0);
-}
-
-t_env	*find_in_env(t_env *env, char *str)
-{
-	t_env	*save;
-
-	save = env;
-	while (save->prev)
-		save = save->prev;
-	while (save->next)
-	{
-		if (ft_strcmp(save->line, str))
-			return (save);
-		save = save->next;
-	}
-	return (NULL);
-}
-	cmds;
+	t_cmds			*cmds;
 	int				fd_in;
 	int				fd_out;
 	int				pipe_fd[2];
@@ -78,5 +47,82 @@ typedef struct s_all
 	t_env	*env;
 	int		status;
 }	t_all;
+
+
+/*
+** builtin/env_functions.c
+*/
+int				ft_strcmp(char *s1, char *s2);
+t_env			*find_in_env(t_env *env, char *str);
+
+/*
+** builtin/env_parsing.c
+*/
+void			*free_new_env(t_env *env);
+char			*ft_strdup(char *str);
+t_env			*add_lst(t_env *prev);
+t_env			*env_to_struct(char **env);
+
+/*
+** builtin/ft_cd.c
+*/
+char			*get_pwd(char *prefix);
+void			put_pwd(t_env *env, char **pwd, int i);
+int				get_home(t_env *env, char **arg);
+int				ft_cd(t_env *env, char *arg);
+
+/*
+** builtin/ft_echo.c
+*/
+int				is_n(char *flag);
+void			ft_echo(char **str);
+
+/*
+** builtin/ft_env.c
+*/
+void			ft_env(t_env *env);
+
+/*
+** builtin/ft_export.c
+*/
+int				print_export(t_env *env, int i);
+int				add_lst_str(t_env *prev, char *var);
+char			*get_var_name(char *str);
+void			modify_line(t_env *env, char *new_line);
+int				ft_export(t_env *env, char *new_var);
+
+/*
+** builtin/ft_pwd.c
+*/
+int				ft_pwd(void);
+
+/*
+** builtin/ft_unset.c
+*/
+void			remove_node(t_env *node);
+int				ft_unset(t_env *env, char *var_name);
+
+/*
+** utils/char.c
+*/
+int				is_alpha(char c);
+int				is_digit(char c);
+int				is_alphanum(char c);
+void			is_in_quote(char c, int *sq, int *dq);
+
+/*
+** utils/free_functions.c
+*/
+void			free_env(t_env *env);
+void			free_cmds(t_cmds *cmds);
+void			free_data(t_data *data);
+void			free_all(t_all *all);
+
+/*
+** utils/strcpy.c
+*/
+size_t			ft_strlen(char *line);
+char			*ft_strcat(char *str_a, char *str_b, int flag, int i);
+char			*ft_strndup(char *str, int n);
 
 #endif
